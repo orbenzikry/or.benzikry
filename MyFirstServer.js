2585773e7c4be62276b2e359d68c74e671d31be8
orbenzikry <orbenzikry@gmail.com>
2021-09-18 14:38:22 +0300

    exercise 6

--- MyFirstServer.js
+++ MyFirstServer.js
@@ -1,5 +1,42 @@
-var http = require('http');
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/html'});
-res.end('Hello World!');
-}).listen(8080);
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const sql = require("./db");
+
+
+
+// parse requests of contenttype: application/json
+app.use(bodyParser.json());
+// parse requests of contenttype: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true
+}));
+
+
+
+// Create a route for getting all customers
+app.get("/customers", function(req, res){
+    sql.query("SELECT * FROM customers", (err, mysqlres) => {
+    if (err) {
+    console.log("error: ", err);
+    res.status(400).send({message: "error in getting all customers: " + err});
+    return;
+    }
+    console.log("got all customers...");
+    res.send(mysqlres);
+    return;
+    });
+    });
+
+
+
+
+// simple route
+app.get("/", (req, res) => {
+res.json({ message: "Welcome to web course example application." });
+});
+
+// set port, listen for requests
+app.listen(3000, () => {
+console.log("Server is running on port 3000."
+);
+});
